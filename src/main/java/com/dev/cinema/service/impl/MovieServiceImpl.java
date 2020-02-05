4f3519e2de9d63d89074c2fae72b52e49cf9b8f9
Yaroslav <yaroslav2875@i.ua>
2020-02-05 16:13:07 +0200

    hw 26 solution

---
+++ src/main/java/com/dev/cinema/service/impl/MovieServiceImpl.java
@@ -0,0 +1,23 @@
+package com.dev.cinema.service.impl;
+
+import com.dev.cinema.dao.MovieDao;
+import com.dev.cinema.lib.Inject;
+import com.dev.cinema.model.Movie;
+import com.dev.cinema.service.MovieService;
+import java.util.List;
+
+public class MovieServiceImpl implements MovieService {
+
+    @Inject
+    private static MovieDao movieDao;
+
+    @Override
+    public Movie add(Movie movie) {
+        return movieDao.add(movie);
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        return movieDao.getAll();
+    }
+}
